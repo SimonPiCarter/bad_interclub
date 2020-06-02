5ea255cec4606c53895983c00f36debc11beca93
Simon PIERRE <simon.pierre@eurodecision.com>
2020-06-02 18:34:38 +0200

    removed old SAT

--- src/test.py
+++ src/test.py
@@ -13,32 +13,6 @@ from opt.variableBuilder import *
 from dto.matchReader import *
 from dto.playerReader import *
 
-def SimpleSatProgram():
-    """Minimal CP-SAT example to showcase calling the solver."""
-    # Creates the model.
-    model = cp_model.CpModel()
-
-    # Creates the variables.
-    num_vals = 3
-    x = model.NewIntVar(0, num_vals - 1, 'x')
-    y = model.NewIntVar(0, num_vals - 1, 'y')
-    z = model.NewIntVar(0, num_vals - 1, 'z')
-
-    # Creates the constraints.
-    model.Add(x != y)
-
-    # Creates the objective.
-    model.Minimize(x+2*y)
-
-    # Creates a solver and solves the model.
-    solver = cp_model.CpSolver()
-    status = solver.Solve(model)
-
-    if status == cp_model.OPTIMAL:
-        print('x = %i' % solver.Value(x))
-        print('y = %i' % solver.Value(y))
-        print('z = %i' % solver.Value(z))
-
 def SimpleMatchProgram():
     """Minimal CP-SAT match."""
     # Creates the model.
